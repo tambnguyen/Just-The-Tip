3b8d921ca2868034d61d31997706a4fa9be8de5e
Tam Nguyen <contact@tambnguyen.com>
2015-05-07 15:46:53 -0400

    initial checkin

---
+++ Just The Tip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Just The Tip
+//
+//  Created by Tam Nguyen on 5/7/15.
+//  Copyright (c) 2015 Tam Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Just The Tip/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Just The Tip
+//
+//  Created by Tam Nguyen on 5/7/15.
+//  Copyright (c) 2015 Tam Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
