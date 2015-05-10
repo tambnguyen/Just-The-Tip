8bac6d2f29c80084d0990f6ff1317926dbd3c1c2
Tam Nguyen <contact@tambnguyen.com>
2015-05-10 12:31:20 -0400

    fixed UI, tidied code

--- Just The Tip/ViewController.m
+++ Just The Tip/ViewController.m
@@ -3,279 +3,272 @@
 //  Just The Tip
 //
 //  Created by Tam Nguyen on 5/7/15.
-//  Copyright (c) 2015 Tam Nguyen. All rights reserved.
+//  Copyright  ( c )  2015 Tam Nguyen. All rights reserved.
 //
 
 #import "ViewController.h"
 #import <QuartzCore/QuartzCore.h>
 
-@interface ViewController ()
+@interface ViewController  (  ) 
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+-  ( void ) viewDidLoad {
+     [ super viewDidLoad ] ;
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.userDefaults = [NSUserDefaults standardUserDefaults];
+    self.userDefaults =  [ NSUserDefaults standardUserDefaults ] ;
     
-    [self getDefaults];
+     [ self getDefaults ] ;
     
-    self.BLUE = [UIColor colorWithRed:(0) green:(122/255.0) blue:(1) alpha:1];
-    self.GRAY = [UIColor colorWithRed:(142/255.0) green:(142/255.0) blue:(147/255.0) alpha:1];
-    self.RED = [UIColor colorWithRed:(1) green:(59/255.0) blue:(48/255.0) alpha:1];
-    self.BLACK = [UIColor colorWithRed:(0) green:(0) blue:(0) alpha:1];
-    self.LIGHTBLUE = [UIColor colorWithRed:(90/255) green:(200/255) blue:(250/255) alpha:1];
-    self.ORANGE = [UIColor colorWithRed:(1) green:(149/255) blue:(0) alpha:1];
+    self.BLUE =  [ UIColor colorWithRed: ( 0 )  green: ( 122/255.0 )  blue: ( 1 )  alpha:1 ] ;
+    self.GRAY =  [ UIColor colorWithRed: ( 142/255.0 )  green: ( 142/255.0 )  blue: ( 147/255.0 )  alpha:1 ] ;
+    self.RED =  [ UIColor colorWithRed: ( 1 )  green: ( 59/255.0 )  blue: ( 48/255.0 )  alpha:1 ] ;
+    self.BLACK =  [ UIColor colorWithRed: ( 0 )  green: ( 0 )  blue: ( 0 )  alpha:1 ] ;
+    self.LIGHTBLUE =  [ UIColor colorWithRed: ( 90/255 )  green: ( 200/255 )  blue: ( 250/255 )  alpha:1 ] ;
+    self.ORANGE =  [ UIColor colorWithRed: ( 1 )  green: ( 149/255 )  blue: ( 0 )  alpha:1 ] ;
     
-    self.arrButtons = @[self.numButton1, self.numButton2, self.numButton3, self.numButton4, self.numButton5, self.numButton6, self.numButton7, self.numButton8, self.numButton9, self.numButton0, self.numButtonDot, self.buttonDel];
+    //self.arrButtons = @ [ self.numButton1, self.numButton2, self.numButton3, self.numButton4, self.numButton5, self.numButton6, self.numButton7, self.numButton8, self.numButton9, self.numButton0, self.numButtonDot, self.buttonDel ] ;
     
-    [self cosmetic: self.arrButtons];
+    self.arrPercent = @ [ @"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30",@"31",@"32",@"33",@"34",@"35",@"36",@"37",@"38",@"39",@"40",@"41",@"42",@"43",@"44",@"45",@"46",@"47",@"48",@"49",@"50" ] ;
+    self.arrPeople = @ [ @"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20" ] ;
     
-    self.arrPercent = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30",@"31",@"32",@"33",@"34",@"35",@"36",@"37",@"38",@"39",@"40",@"41",@"42",@"43",@"44",@"45",@"46",@"47",@"48",@"49",@"50"];
-    self.arrPeople = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20"];
-    
-    self.percent = [NSNumber numberWithFloat:self.default_tip];
-    self.people = [NSNumber numberWithInt:1];
+    self.percent =  [ NSNumber numberWithFloat:self.default_tip ] ;
+    self.people =  [ NSNumber numberWithInt:1 ] ;
     self.strSubTotal = @"";
     
     self.myPicker.dataSource = self;
     self.myPicker.delegate = self;
     
-    if (self.bRememberLastBill)
+    if  ( self.bRememberLastBill ) 
     {
-        self.subtotal = [self.userDefaults floatForKey:@"sub_total"];
-        self.strSubTotal = [NSString stringWithFormat: @"%.2f", self.subtotal];
-        [self updateSubTotal:-3];
+        self.subtotal =  [ self.userDefaults floatForKey:@"sub_total" ] ;
+        self.strSubTotal =  [ NSString stringWithFormat: @"%.2f", self.subtotal ] ;
+         [ self updateSubTotal:-3 ] ;
     }
     
-    [self animate];
+     [ self animate ] ;
 }
 
-- (void)viewDidAppear:(BOOL)animated
+-  ( void ) viewDidAppear: ( BOOL ) animated
 {
-    [super viewDidAppear:animated];
-    [self.myPicker selectRow:self.percent.integerValue-1 inComponent:0 animated:YES];
-    [self.myPicker selectRow:self.people.integerValue-1 inComponent:1 animated:YES];
+     [ super viewDidAppear:animated ] ;
+     [ self.myPicker selectRow:self.percent.integerValue-1 inComponent:0 animated:YES ] ;
+     [ self.myPicker selectRow:self.people.integerValue-1 inComponent:1 animated:YES ] ;
 }
 
-- (void)viewWillAppear:(BOOL)animated
+-  ( void ) viewWillAppear: ( BOOL ) animated
 {
-    [super viewWillAppear:animated];
-    [self animate];
+     [ super viewWillAppear:animated ] ;
+     [ self animate ] ;
 }
 
-- (void) getDefaults
+-  ( void )  getDefaults
 {
-    self.people = [NSNumber numberWithInteger:[self.userDefaults integerForKey:@"default_people"]];
+    self.people =  [ NSNumber numberWithInteger: [ self.userDefaults integerForKey:@"default_people" ]  ] ;
     
-    self.bDefaultRoundTip = [self.userDefaults boolForKey:@"default_roundtip"];
-    self.bDefaultDontRound = [self.userDefaults boolForKey:@"default_dontround"];
-    self.bDefaultRoundTotal = [self.userDefaults boolForKey:@"default_roundtotal"];
-    self.bRememberLastBill = [self.userDefaults boolForKey:@"remember_last_bill"];
-    self.subtotal = [self.userDefaults floatForKey:@"sub_total"];
+    self.bDefaultRoundTip =  [ self.userDefaults boolForKey:@"default_roundtip" ] ;
+    self.bDefaultDontRound =  [ self.userDefaults boolForKey:@"default_dontround" ] ;
+    self.bDefaultRoundTotal =  [ self.userDefaults boolForKey:@"default_roundtotal" ] ;
+    self.bRememberLastBill =  [ self.userDefaults boolForKey:@"remember_last_bill" ] ;
+    self.subtotal =  [ self.userDefaults floatForKey:@"sub_total" ] ;
     
-    self.default_tax = [self.userDefaults boolForKey:@"has_default_tax"] == YES ? [self.userDefaults floatForKey:@"default_tax"] : 8.875;
-    self.default_tip = [self.userDefaults boolForKey:@"has_default_tip"] == YES ? [self.userDefaults floatForKey:@"default_tip"] : 15.0;
+    self.default_tax =  [ self.userDefaults boolForKey:@"has_default_tax" ]  == YES ?  [ self.userDefaults floatForKey:@"default_tax" ]  : 8.875;
+    self.default_tip =  [ self.userDefaults boolForKey:@"has_default_tip" ]  == YES ?  [ self.userDefaults floatForKey:@"default_tip" ]  : 15.0;
     
     self.bRoundTip = self.bDefaultRoundTip;
     self.bRoundTotal = self.bDefaultRoundTotal;
     self.bDontRound = self.bDefaultDontRound;
 }
 
-- (void) animate
+-  ( void )  animate
 {
-    [self.switch_DontRound setOn:self.bDontRound animated:YES];
-    [self.switch_RoundTotal setOn:self.bRoundTotal animated:YES];
-    [self.switch_RoundTip setOn:self.bRoundTip animated:YES];
-    [self.myPicker reloadAllComponents];
+     [ self.switch_DontRound setOn:self.bDontRound animated:YES ] ;
+     [ self.switch_RoundTotal setOn:self.bRoundTotal animated:YES ] ;
+     [ self.switch_RoundTip setOn:self.bRoundTip animated:YES ] ;
+     [ self.myPicker reloadAllComponents ] ;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+-  ( void ) didReceiveMemoryWarning {
+     [ super didReceiveMemoryWarning ] ;
     // Dispose of any resources that can be recreated.
 }
 
-- (void) cosmetic: (NSArray *) arrButtons
-{
-    
-}
-
 // returns the number of 'columns' to display.
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+-  ( NSInteger ) numberOfComponentsInPickerView: ( UIPickerView * ) pickerView
 {
     return 2;
 }
 
 // returns the # of rows in each component..
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component
+-  ( NSInteger ) pickerView: ( UIPickerView * ) pickerView numberOfRowsInComponent:  ( NSInteger ) component
 {
-    if(component== 0)
+    if ( component== 0 ) 
     {
-        return [self.arrPercent count];
+        return  [ self.arrPercent count ] ;
     }
     else
     {
-        return [self.arrPeople count];
+        return  [ self.arrPeople count ] ;
     }
 }
 
--(NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component
+- ( NSAttributedString * ) pickerView: ( UIPickerView * ) pickerView attributedTitleForRow: ( NSInteger ) row forComponent: ( NSInteger ) component
 {
     NSString *title;
     NSAttributedString *attString;
-    if(component == 0)
+    if ( component == 0 ) 
     {
-        title = [NSString stringWithFormat:@"%@%@", [self.arrPercent objectAtIndex:row], @"%"];
+        title =  [ NSString stringWithFormat:@"%@%@",  [ self.arrPercent objectAtIndex:row ] , @"%" ] ;
     }
     else
     {
-        title = [self.arrPeople objectAtIndex:row];
+        title =  [ self.arrPeople objectAtIndex:row ] ;
     }
-    attString = [[NSAttributedString alloc] initWithString:title attributes:@{NSForegroundColorAttributeName: self.ORANGE}];
+    attString =  [ [ NSAttributedString alloc ] initWithString:title attributes:@{NSForegroundColorAttributeName: self.ORANGE} ] ;
     return attString;
 }
 
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+-  ( void ) pickerView: ( UIPickerView * ) pickerView didSelectRow: ( NSInteger ) row inComponent: ( NSInteger ) component
 {
-    if(component == 0)
+    if ( component == 0 ) 
     {
-        self.percent = [self.arrPercent objectAtIndex:row];
+        self.percent =  [ self.arrPercent objectAtIndex:row ] ;
     }
     else
     {
-        self.people = [self.arrPeople objectAtIndex:row];
+        self.people =  [ self.arrPeople objectAtIndex:row ] ;
     }
-    [self updateSubTotal:-3];
+     [ self updateSubTotal:-3 ] ;
 }
 
-- (void) updateSubTotal: (float) value
+-  ( void )  updateSubTotal:  ( float )  value
 {
-    NSString *strTotal = self.field_SubTotal.text.length > 0 ? [self.field_SubTotal.text stringByReplacingOccurrencesOfString:@"$ " withString:@""] : NULL;
+    NSString *strTotal = self.field_SubTotal.text.length > 0 ?  [ self.field_SubTotal.text stringByReplacingOccurrencesOfString:@"$ " withString:@"" ]  : NULL;
     
-    if (value == -1) {  // dot
-        if (strTotal.length > 0) { self.strSubTotal = [NSString stringWithFormat:@"%@%@", strTotal, @"."]; }
+    if  ( value == -1 )  {  // dot
+        if  ( strTotal.length > 0 )  { self.strSubTotal =  [ NSString stringWithFormat:@"%@%@", strTotal, @"." ] ; }
     }
-    else if (value == -2) { // del
-        if (strTotal.length > 0) { self.strSubTotal = [strTotal substringToIndex:[strTotal length]-1]; }
+    else if  ( value == -2 )  { // del
+        if  ( strTotal.length > 0 )  { self.strSubTotal =  [ strTotal substringToIndex: [ strTotal length ] -1 ] ; }
     }
-    else if (value == -3) {
-        NSLog(@"No op!");
+    else if  ( value == -3 )  {
+        NSLog ( @"No op!" ) ;
     }
     else {
-        if (strTotal.length == 0 && value != 0) {
-            self.strSubTotal = [NSString stringWithFormat:@"%d", (int)value];
+        if  ( strTotal.length == 0 && value != 0 )  {
+            self.strSubTotal =  [ NSString stringWithFormat:@"%d",  ( int ) value ] ;
         }
-        else if (strTotal.length == 0 && value == 0) {
+        else if  ( strTotal.length == 0 && value == 0 )  {
             self.strSubTotal = @"";
         }
         else {
-            self.strSubTotal = [NSString stringWithFormat:@"%@%d", strTotal, (int)value];
+            self.strSubTotal =  [ NSString stringWithFormat:@"%@%d", strTotal,  ( int ) value ] ;
         }
     }
-    self.field_SubTotal.text = [NSString stringWithFormat:@"$ %@", self.strSubTotal];
+    self.field_SubTotal.text =  [ NSString stringWithFormat:@"$ %@", self.strSubTotal ] ;
     
     self.subtotal = self.strSubTotal.floatValue;
-    [self.userDefaults setFloat:self.strSubTotal.floatValue forKey:@"sub_total"];
-    [self calcTip: self.subtotal];
-    [self calcTotal: self.subtotal];
+     [ self.userDefaults setFloat:self.strSubTotal.floatValue forKey:@"sub_total" ] ;
+     [ self calcTip: self.subtotal ] ;
+     [ self calcTotal: self.subtotal ] ;
 }
 
-- (void) calcTotal: (float) subtotal
+-  ( void )  calcTotal:  ( float )  subtotal
 {
-    self.total = (self.strSubTotal.floatValue + self.tip) / self.people.intValue;
-    if (self.bRoundTotal)
+    self.total =  ( self.strSubTotal.floatValue + self.tip )  / self.people.intValue;
+    if  ( self.bRoundTotal ) 
     {
-        self.total = ceilf(self.total);
+        self.total = ceilf ( self.total ) ;
     }
-    self.field_Total.text = [NSString stringWithFormat:@"$ %.2f", self.total];
+    self.field_Total.text =  [ NSString stringWithFormat:@"$ %.2f", self.total ] ;
 }
 
-- (void) calcTip: (float) subtotal
+-  ( void )  calcTip:  ( float )  subtotal
 {
     self.tip = subtotal * self.percent.floatValue / 100.0;
-    if (self.bRoundTip)
+    if  ( self.bRoundTip ) 
     {
-        self.tip = (float)ceilf(self.tip);
+        self.tip =  ( float ) ceilf ( self.tip ) ;
     }
-    self.field_Tip.text = [NSString stringWithFormat:@"$ %.2f", self.tip];
+    self.field_Tip.text =  [ NSString stringWithFormat:@"$ %.2f", self.tip ] ;
 }
 
-- (IBAction)touchDown_b0:(id)sender {
-    [self updateSubTotal:0.0];
+-  ( IBAction ) touchDown_b0: ( id ) sender {
+     [ self updateSubTotal:0.0 ] ;
 }
 
-- (IBAction)touchDown_b1:(id)sender {
-    [self updateSubTotal:1.0];
+-  ( IBAction ) touchDown_b1: ( id ) sender {
+     [ self updateSubTotal:1.0 ] ;
 }
 
-- (IBAction)touchDown_b2:(id)sender {
-    [self updateSubTotal:2.0];
+-  ( IBAction ) touchDown_b2: ( id ) sender {
+     [ self updateSubTotal:2.0 ] ;
 }
 
-- (IBAction)touchDown_b3:(id)sender {
-    [self updateSubTotal:3.0];
+-  ( IBAction ) touchDown_b3: ( id ) sender {
+     [ self updateSubTotal:3.0 ] ;
 }
 
-- (IBAction)touchDown_b4:(id)sender {
-    [self updateSubTotal:4.0];
+-  ( IBAction ) touchDown_b4: ( id ) sender {
+     [ self updateSubTotal:4.0 ] ;
 }
 
-- (IBAction)touchDown_b5:(id)sender {
-    [self updateSubTotal:5.0];
+-  ( IBAction ) touchDown_b5: ( id ) sender {
+     [ self updateSubTotal:5.0 ] ;
 }
 
-- (IBAction)touchDown_b6:(id)sender {
-    [self updateSubTotal:6.0];
+-  ( IBAction ) touchDown_b6: ( id ) sender {
+     [ self updateSubTotal:6.0 ] ;
 }
 
-- (IBAction)touchDown_b7:(id)sender {
-    [self updateSubTotal:7.0];
+-  ( IBAction ) touchDown_b7: ( id ) sender {
+     [ self updateSubTotal:7.0 ] ;
 }
 
-- (IBAction)touchDown_b8:(id)sender {
-    [self updateSubTotal:8.0];
+-  ( IBAction ) touchDown_b8: ( id ) sender {
+     [ self updateSubTotal:8.0 ] ;
 }
 
-- (IBAction)touchDown_b9:(id)sender {
-    [self updateSubTotal:9.0];
+-  ( IBAction ) touchDown_b9: ( id ) sender {
+     [ self updateSubTotal:9.0 ] ;
 }
 
-- (IBAction)touchDown_dot:(id)sender {
-    [self updateSubTotal:-1];
+-  ( IBAction ) touchDown_dot: ( id ) sender {
+     [ self updateSubTotal:-1 ] ;
 }
 
-- (IBAction)touchDown_del:(id)sender {
-    [self updateSubTotal:-2];
+-  ( IBAction ) touchDown_del: ( id ) sender {
+     [ self updateSubTotal:-2 ] ;
 }
 
-- (IBAction)up_RoundTip:(id)sender {
+-  ( IBAction ) up_RoundTip: ( id ) sender {
     self.bRoundTip = self.switch_RoundTip.isOn;
     self.bDontRound = !self.bRoundTip && self.bRoundTip;
     self.bRoundTotal = !self.bRoundTip && self.bRoundTip;
     
-    [self animate];
-    [self updateSubTotal:-3];
+     [ self animate ] ;
+     [ self updateSubTotal:-3 ] ;
 }
 
-- (IBAction)up_RoundTotal:(id)sender {
+-  ( IBAction ) up_RoundTotal: ( id ) sender {
     self.bRoundTotal = self.switch_RoundTotal.isOn;
     self.bDontRound = !self.bRoundTotal && self.bRoundTotal;
     self.bRoundTip = !self.bRoundTotal && self.bRoundTotal;
     
-    [self animate];
-    [self updateSubTotal:-3];
+     [ self animate ] ;
+     [ self updateSubTotal:-3 ] ;
 }
 
-- (IBAction)up_DontRound:(id)sender {
+-  ( IBAction ) up_DontRound: ( id ) sender {
     self.bDontRound = TRUE;
     self.bRoundTip = FALSE;
     self.bRoundTotal = FALSE;
     
-    [self animate];
-    [self updateSubTotal:-3];
+     [ self animate ] ;
+     [ self updateSubTotal:-3 ] ;
 }
 
 @end
--- Just The Tip/mySettings.m
+++ Just The Tip/mySettings.m
@@ -3,33 +3,33 @@
 //  Just The Tip
 //
 //  Created by Tam Nguyen on 5/9/15.
-//  Copyright (c) 2015 Tam Nguyen. All rights reserved.
+//  Copyright  ( c )  2015 Tam Nguyen. All rights reserved.
 //
 
 #import "mySettings.h"
 
-@interface mySettings ()
+@interface mySettings  (  ) 
 
 @end
 
 @implementation mySettings
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+-  ( void ) viewDidLoad {
+     [ super viewDidLoad ] ;
     // Do any additional setup after loading the view.
     
-    self.userDefaults = [NSUserDefaults standardUserDefaults];
+    self.userDefaults =  [ NSUserDefaults standardUserDefaults ] ;
     
-    [self getDefaults];
+     [ self getDefaults ] ;
     
-    self.textDefaultTax.text = [NSString stringWithFormat:@"%.3f", self.default_tax];
-    self.textDefaultTip.text = [NSString stringWithFormat:@"%.f", self.default_tip];
+    self.textDefaultTax.text =  [ NSString stringWithFormat:@"%.3f", self.default_tax ] ;
+    self.textDefaultTip.text =  [ NSString stringWithFormat:@"%.f", self.default_tip ] ;
     
-    [self animate];
+     [ self animate ] ;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+-  ( void ) didReceiveMemoryWarning {
+     [ super didReceiveMemoryWarning ] ;
     // Dispose of any resources that can be recreated.
 }
 
@@ -37,87 +37,87 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
+-  ( void ) prepareForSegue: ( UIStoryboardSegue * ) segue sender: ( id ) sender {
+    // Get the new view controller using  [ segue destinationViewController ] .
     // Pass the selected object to the new view controller.
 }
 */
 
-- (void) getDefaults
+-  ( void ) getDefaults
 {
-    self.bDefaultRoundTip = [self.userDefaults boolForKey:@"default_roundtip"];
-    self.bDefaultDontRound = [self.userDefaults boolForKey:@"default_dontround"];
-    self.bDefaultRoundTotal = [self.userDefaults boolForKey:@"default_roundtotal"];
-    self.bRememberLastBill = [self.userDefaults boolForKey:@"remember_last_bill"];
+    self.bDefaultRoundTip =  [ self.userDefaults boolForKey:@"default_roundtip" ] ;
+    self.bDefaultDontRound =  [ self.userDefaults boolForKey:@"default_dontround" ] ;
+    self.bDefaultRoundTotal =  [ self.userDefaults boolForKey:@"default_roundtotal" ] ;
+    self.bRememberLastBill =  [ self.userDefaults boolForKey:@"remember_last_bill" ] ;
     
-    self.default_tax = [self.userDefaults boolForKey:@"has_default_tax"] == YES ? [self.userDefaults floatForKey:@"default_tax"] : 8.875;
-    self.default_tip = [self.userDefaults boolForKey:@"has_default_tip"] == YES ? [self.userDefaults floatForKey:@"default_tip"] : 15.0;
+    self.default_tax =  [ self.userDefaults boolForKey:@"has_default_tax" ]  == YES ?  [ self.userDefaults floatForKey:@"default_tax" ]  : 8.875;
+    self.default_tip =  [ self.userDefaults boolForKey:@"has_default_tip" ]  == YES ?  [ self.userDefaults floatForKey:@"default_tip" ]  : 15.0;
 }
 
-- (void) setDefaults
+-  ( void ) setDefaults
 {
-    [self.userDefaults setBool:self.bDefaultRoundTip forKey:@"default_roundtip"];
-    [self.userDefaults setBool:self.bDefaultDontRound forKey:@"default_dontround"];
-    [self.userDefaults setBool:self.bDefaultRoundTotal forKey:@"default_roundtotal"];
-    [self.userDefaults setBool:self.bRememberLastBill forKey:@"remember_last_bill"];
+     [ self.userDefaults setBool:self.bDefaultRoundTip forKey:@"default_roundtip" ] ;
+     [ self.userDefaults setBool:self.bDefaultDontRound forKey:@"default_dontround" ] ;
+     [ self.userDefaults setBool:self.bDefaultRoundTotal forKey:@"default_roundtotal" ] ;
+     [ self.userDefaults setBool:self.bRememberLastBill forKey:@"remember_last_bill" ] ;
     
-    [self.userDefaults setFloat:self.default_tax forKey:@"default_tax"];
-    [self.userDefaults setBool:YES forKey:@"has_default_tax"];
+     [ self.userDefaults setFloat:self.default_tax forKey:@"default_tax" ] ;
+     [ self.userDefaults setBool:YES forKey:@"has_default_tax" ] ;
     
-    [self.userDefaults setFloat:self.default_tip forKey:@"default_tip"];
-    [self.userDefaults setBool:YES forKey:@"has_default_tip"];
+     [ self.userDefaults setFloat:self.default_tip forKey:@"default_tip" ] ;
+     [ self.userDefaults setBool:YES forKey:@"has_default_tip" ] ;
     
-    [self.userDefaults synchronize];
+     [ self.userDefaults synchronize ] ;
 }
 
-- (void) animate
+-  ( void ) animate
 {
-    [self.switchDefaultDontRound setOn:self.bDefaultDontRound animated:YES];
-    [self.switchDefaultRoundTotal setOn:self.bDefaultRoundTotal animated:YES];
-    [self.switchDefaultRoundTip setOn:self.bDefaultRoundTip animated:YES];
-    [self.switchRememberLastBill setOn:self.bRememberLastBill animated:YES];
+     [ self.switchDefaultDontRound setOn:self.bDefaultDontRound animated:YES ] ;
+     [ self.switchDefaultRoundTotal setOn:self.bDefaultRoundTotal animated:YES ] ;
+     [ self.switchDefaultRoundTip setOn:self.bDefaultRoundTip animated:YES ] ;
+     [ self.switchRememberLastBill setOn:self.bRememberLastBill animated:YES ] ;
 }
 
-- (IBAction)up_default_roundtip:(id)sender {
+-  ( IBAction ) up_default_roundtip: ( id ) sender {
     self.bDefaultRoundTip = self.switchDefaultRoundTip.isOn;
     self.bDefaultDontRound = !self.bDefaultRoundTip && self.bDefaultRoundTip;
     self.bDefaultRoundTotal = !self.bDefaultRoundTip && self.bDefaultRoundTip;
 
-    [self setDefaults];
-    [self animate];
+     [ self setDefaults ] ;
+     [ self animate ] ;
 }
 
-- (IBAction)up_default_roundtotal:(id)sender {
+-  ( IBAction ) up_default_roundtotal: ( id ) sender {
     self.bDefaultRoundTotal = self.switchDefaultRoundTotal.isOn;
     self.bDefaultDontRound = !self.bDefaultRoundTotal && self.bDefaultRoundTotal;
     self.bDefaultRoundTip = !self.bDefaultRoundTotal && self.bDefaultRoundTotal;
 
-    [self setDefaults];
-    [self animate];
+     [ self setDefaults ] ;
+     [ self animate ] ;
 }
 
-- (IBAction)up_default_dontround:(id)sender {
+-  ( IBAction ) up_default_dontround: ( id ) sender {
     self.bDefaultDontRound = TRUE;
     self.bDefaultRoundTip = FALSE;
     self.bDefaultRoundTotal = FALSE;
     
-    [self setDefaults];
-    [self animate];
+     [ self setDefaults ] ;
+     [ self animate ] ;
 }
 
-- (IBAction)up_default_remember_last_bill:(id)sender {
+-  ( IBAction ) up_default_remember_last_bill: ( id ) sender {
     self.bRememberLastBill = self.switchRememberLastBill.isOn;
-    [self setDefaults];
+     [ self setDefaults ] ;
 }
 
-- (IBAction)up_default_tax:(id)sender {
+-  ( IBAction ) up_default_tax: ( id ) sender {
     self.default_tax = self.textDefaultTax.text.floatValue;
-    [self setDefaults];
+     [ self setDefaults ] ;
 }
 
-- (IBAction)up_default_tip:(id)sender {
+-  ( IBAction ) up_default_tip: ( id ) sender {
     self.default_tip = self.textDefaultTip.text.floatValue;
-    [self setDefaults];
+     [ self setDefaults ] ;
 }
 
 @end
